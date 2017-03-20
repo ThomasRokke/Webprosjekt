6f30a60bd1720cfdd95136eaa8f88fb7aa52ed3e
Thomas Røkke <thomas.roekke@gmail.com>
2017-03-20 22:51:13 +0100

    Navigasjonsbaren har fått href verdi, som liker sammen subject / page ID med riktig url. På denne måten kan vi automatisk vise innhold som er på siden ut i fra navigasjonsmenyen

--- my_company/includes/functions.php
+++ my_company/includes/functions.php
@@ -6,6 +6,37 @@
         }
     }
 
+    function find_all_subjects(){
+        
+        global $connection; 
+        
+        $query  = "SELECT * ";
+        $query .= "FROM subjects ";
+        $query .= "WHERE visible = 1 ";
+        $query .= "ORDER BY position ASC";
+        $result = mysqli_query($connection, $query);
+        //test if there was a querry error using function.
+        confirm_query($result); 
+        
+        return $result; 
+    }
+
+    function find_pages_for_subject($subject_id){
+        
+        global $connection; 
+         
+        $query  = "SELECT * ";
+        $query .= "FROM pages ";
+        $query .= "WHERE visible = 1 ";
+        $query .= "AND subject_id = {$subject_id} ";
+        $query .= "ORDER BY position ASC"; 
+
+        $pages_result = mysqli_query($connection, $query);
+        //test if there was a querry error using function.
+        confirm_query($pages_result);  
+        
+        return $pages_result; 
+    }
     
 
 ?>
