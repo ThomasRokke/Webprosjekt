6d9b53ab10be24e1e5353cc68f5c8457cc964a7b
Thomas Røkke <thomas.roekke@gmail.com>
2017-03-20 16:28:51 +0100

    Har påbegynt et nytt prosjekt for å teste å separere admin og bruker del av side. /m innloggnig og koblet opp mot database. Ligger i mappen my_company

---
+++ my_company/public/admin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>New HMTL document by NewJect</title>
+
+</head>
+<body>
+
+    <div id="header">
+        <h1>My Company</h1>
+    </div>
+    
+    <div id="main">
+        <div id="navigation">
+            &nbsp;
+        </div>
+        <div id="page">
+            <h2>Admin menu</h2>
+            <p>Welcome to the admin area.</p>
+            <ul>
+                <li><a href="manage_content.php">Manage Website</a></li>
+
+                <li><a href="manage_admins.php">Manage admin</a></li>
+            
+                <li><a href="logout.php">Logout</a></li>
+            
+            </ul>
+        </div>
+    </div>
+    
+    <div id="footer">Copyright 2017, Thomas Røkke</div>
+
+</body>
+</html>
