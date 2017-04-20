ffdeeacbd269f273b624672ff99c6386139a0a68
Thomas Røkke <thomas.roekke@gmail.com>
2017-04-20 11:24:50 +0200

    Lagde mal

---
+++ DesignForslag/FellesForslag/public/mal.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+    <link href="CSS/index.css" type="text/css" rel="stylesheet">
+
+
+<title>Felles Designforslag</title>
+
+</head>
+<body>
+
+<?php include_once("../includes/filnavn.php"); ?>
+
+
+
+</body>
+</html>
