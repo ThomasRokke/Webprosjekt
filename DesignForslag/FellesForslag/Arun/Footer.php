17fcbb18149db1fe777966bb0b1cc61b49e2c0dd
arunsangha <arun.sangha@gmail.com>
2017-05-10 13:32:41 +0200

    Om oss

---
+++ DesignForslag/FellesForslag/Arun/Footer.php
@@ -0,0 +1,4 @@
+
+<div id ="footer">
+<h3 id="footerTekst">@2017 WZUP?</h3>
+</div>
---
+++ DesignForslag/FellesForslag/Arun/omoss.php
@@ -0,0 +1,66 @@
+<html>
+    <head> 
+        <link rel="stylesheet" type="text/css" href="omOss.css">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+    </head>
+    <body> 
+        <title> wZup Fjerdingen</title>
+            <div id="navigation">
+                 
+                 <div class="naviBoks">
+                    <ul>
+                     <li><a class="active" href="#home">Home</a></li>
+                     <li><a href="#events">Events</a></li>
+                     <li><a href="#food">Food and drinks</a></li>
+                     <li><a href="#bars">Bars</a></li>
+                   </ul>
+                 </div>
+                
+            </div>
+             
+                    
+           <div id ="forsidebilde"></div>  
+            <div id="mainContainer">
+                <div id="hvem">Hvem er vi?</div>
+                <div class ="oss">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Small-city-symbol.svg/348px-Small-city-symbol.svg.png" alt="Person1" class="image">
+                   <div class="overlay"> 
+                   <div class="text">Thomas Røkke</div>
+                   </div>
+               </div>
+                <div class ="oss">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Small-city-symbol.svg/348px-Small-city-symbol.svg.png" alt="Person2" class="image">
+                   <div class="overlay"> 
+                   <div class="text">Arun Sangha</div>
+                   </div>
+               </div>
+                <div class ="oss">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Small-city-symbol.svg/348px-Small-city-symbol.svg.png" alt="Person2" class="image">
+                   <div class="overlay"> 
+                   <div class="text">Christian Thorby</div>
+                   </div>
+               </div>
+                <div class ="oss">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Small-city-symbol.svg/348px-Small-city-symbol.svg.png" alt="Person2" class="image">
+                   <div class="overlay"> 
+                   <div class="text">Fredrik Muller</div>
+                   </div>
+               </div>
+                <div class ="oss">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Small-city-symbol.svg/348px-Small-city-symbol.svg.png" alt="Person2" class="image">
+                   <div class="overlay"> 
+                   <div class="text">Michael Fredriksen</div>
+                   </div>
+               </div>
+            
+        </div>
+          <?php include("Footer.php")?>
+             
+    </body>
+
+
+
+
+
+</html> 
