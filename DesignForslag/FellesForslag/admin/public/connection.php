c43a8f760caf7af83d4aab7ec7a2ef8b37684c56
Christian Thorby <christianthorby@hotmail.com>
2017-05-15 15:29:07 +0200

    har lagt til undersider + underside 'utesteder' henter nå info fra DB

--- DesignForslag/FellesForslag/admin/public/connection.php
+++ DesignForslag/FellesForslag/admin/public/connection.php
@@ -6,7 +6,7 @@
 	define("DB_NAME", "wzup");
 
 //mysqli
-$mysqli = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
 if (mysqli_connect_errno()) 
 {
