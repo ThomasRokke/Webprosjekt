f90fd3815c8a2eab59daa1345710a5886e66b5e6
Christian Thorby <christianthorby@hotmail.com>
2017-05-15 15:29:07 +0200

    har lagt til undersider + underside 'utesteder' henter nå info fra DB

--- DesignForslag/FellesForslag/admin/Includes/menu.php
+++ DesignForslag/FellesForslag/admin/Includes/menu.php
@@ -2,16 +2,19 @@
 <div id="menu-div">
     <ul class="menu">
         <li>
-            <a href="">Utesteder</a>
+            <a href="../public/Index.php">Hjem</a>
         </li>
         <li>
-            <a href="">Mat og drikke</a>
+            <a href="../public/utesteder.php">Utesteder</a>
         </li>
         <li>
-            <a href="">Aktiviteter</a>
+            <a href="../public/matogdrikke.php">Mat og drikke</a>
         </li>
         <li>
-            <a href="">Om oss</a>
+            <a href="../public/aktiviteter.php">Aktiviteter</a>
+        </li>
+        <li>
+            <a href="../public/omoss.php">Om oss</a>
         </li>
     </ul>
 </div>
