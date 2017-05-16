e17ad857c3e50505678c6fe87a257794ec080199
Thomas Røkke <thomas.roekke@gmail.com>
2017-05-16 15:07:55 +0200

    Videre arbeid med Live mappe. Nå fungerer admin form --> DB. Kart med markers fra database. Og bokser med info blir automatisk generert ved hjelp av DB - Thomas Røkke

---
+++ Live/public/getBox.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas
+ * Date: 15.05.2017
+ * Time: 10.53
+ */
+
+require_once('../Includes/db_tilkobling.php');
+
+$query = "SELECT name, imagepath, sDesc FROM markers";
+
+$response = @mysqli_query($dbc, $query);
+
+# City = bilde, first_name = tittel, last_name = beskrivelse
+if($response){
+// mysqli_fetch_array will return a row of data from the query
+// until no further data is available
+    while($row = mysqli_fetch_array($response)) {
+
+
+
+
+        echo '<div class="CTbox" id="festbox">' .
+            '<div class="CTbox-image">' .
+            '<img src="Images/'.
+             $row['imagepath'] .
+            '">' .
+            '</div>' .
+
+            '<div class="CTbox-info">' .
+            '<h4>'.
+             $row['name'].
+            '</h4>' .
+            '<p>'.
+            $row['sDesc'].
+            '</p>' .
+            '<a class="CTbutton" href="#fest">Les mer!</a>' .
+            '</div>' .
+            '</div>';
+        }
+
+
+}
+else {
+
+    echo "Couldn't issue database query<br />";
+
+    echo mysqli_error($dbc);
+
+}
+
+// Close connection to the database
+mysqli_close($dbc);
+
+
+
+
+
+
+
