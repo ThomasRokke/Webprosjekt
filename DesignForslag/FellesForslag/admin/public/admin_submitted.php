92c0ba769fec09334a3e217ccc44495575fbd47d
Thomas Røkke <thomas.roekke@gmail.com>
2017-05-16 12:34:09 +0200

    Admin form completed og koblet til database. Hovedside med bokser som automatisk genereres er også lagd.

---
+++ DesignForslag/FellesForslag/admin/public/admin_submitted.php
@@ -0,0 +1,60 @@
+<?php include("../includes/head.php"); ?>
+
+
+<?php include("../includes/menu.php"); ?>
+
+<div class="top-image">
+</div>
+
+
+
+<section class="main-content">
+
+    <?php
+    include_once('studentadded.php');
+    ?>
+
+
+
+
+
+</section>
+
+
+<footer class="TRfooter">
+    <p class="footerText">&copy; 2017 WZup?<p>
+    <p class="footerAdr">Chr. Krohgs gate 32, 0186 Oslo</p>
+
+</footer>
+
+
+
+
+<!-- Javascript -->
+
+<script>
+    function initMap(){
+        var location = {lat: 59.913869, lng: 10.752245};
+        var mapOptions = {
+            zoom: 14,
+            left: location,
+            scrollwheel: false
+        }
+
+        var map = new google.maps.Map(document.getElementById('map'),mapOptions
+        )
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        })
+    };
+</script>
+
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAUOSBwWQ1fKQHF-40kF5kK4h_h4VYA46U&callback=initMap">
+</script>
+
+
+</body>
+</html>
