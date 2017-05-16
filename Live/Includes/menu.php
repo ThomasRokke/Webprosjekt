7fd6a2d8976f7f03f3ce3431de2e54575e42bb74
Thomas Røkke <Thomas Rkke>
2017-05-16 13:29:15 +0200

    Live mappe som skal fungere som innleveringen er opprettet med riktig struktur

---
+++ Live/Includes/menu.php
@@ -0,0 +1,20 @@
+   
+<div id="menu-div">
+    <ul class="menu">
+        <li>
+            <a href="../public/Index.php">Hjem</a>
+        </li>
+        <li>
+            <a href="../public/utesteder.php">Utesteder</a>
+        </li>
+        <li>
+            <a href="../public/matogdrikke.php">Mat og drikke</a>
+        </li>
+        <li>
+            <a href="../public/aktiviteter.php">Aktiviteter</a>
+        </li>
+        <li>
+            <a href="../public/omoss.php">Om oss</a>
+        </li>
+    </ul>
+</div>
