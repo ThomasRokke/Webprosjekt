ce512507d5130ac720f3d5993c30e2cc9a794f4c
Thomas Røkke <Thomas Rkke>
2017-05-18 13:48:25 +0200

    Fikset liten bugg der form ble slettet ved å trykke <finn koordinater knappen> - løsning var å legge utenfor form/sections tags

--- Live/public/admin.php
+++ Live/public/admin.php
@@ -7,18 +7,73 @@
     </div>
 
 
-    
-    <section class="main-content">
-        
-    <?php
-            include_once('addstudent.php');
-    ?>
 
-        
-        
-        
-        
+
+
+    <section class="main-content">
+
+        <button onclick="findLatLng()">Finn koordinater</button>
+
+        <form action="/Web/Webprosjekt-test/Live/public/adddcontent.php" method="post" autocomplete="on">
+
+            <b>Legg til innhold</b>
+
+            <p>Navn:
+                <input type="text" name="name" size="30" maxlength="60" value="" />
+            </p>
+
+            <p>Marker info:
+                <input type="text" name="address" size="30" maxlength="85" value="" />
+            </p>
+
+
+
+            <p>lat(lengdegrad):
+                <input type="text" name="lat" size="30" maxlength="60" value="" />
+            </p>
+
+            <p>lng(breddegrad):
+                <input type="text" name="lng" size="30" maxlength="60" value="" />
+            </p>
+
+            <p>Type:
+                <input type="text" name="type" size="30" maxlength="30" value="" />
+            </p>
+
+            <p>Kort beskrivelse:
+                <input type="text" name="sDesc" size="30" maxlength="90" value="" />
+            </p>
+
+            <p>Beskrivelse:
+                <input type="text" name="description" size="30" maxlength="500" value="" />
+            </p>
+
+            <p>Filnavn på bilde:
+                <input type="text" name="imagepath" size="30" maxlength="60" value="" />
+            </p>
+
+            <p>
+                <input type="submit" name="submit" value="Send" />
+            </p>
+
+        </form>
+
+
+
+
+
+
     </section>
+
+<button onclick="findLatLng()">Finn koordinater</button>
+
+<script type="text/javascript">
+
+    function findLatLng() {
+        var mittVindu = window.open("http://www.latlong.net/convert-address-to-lat-long.html", "", "width=800,height=600,titlebar=no");
+    }
+
+</script>
 
 
     <footer class="TRfooter">
