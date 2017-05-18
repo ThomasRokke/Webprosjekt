257f26513d07be9c6279396ffcf25a1338d5628d
Thomas Røkke <Thomas Rkke>
2017-05-18 14:31:51 +0200

    Undersider på LIVE folder er opprettet

--- Live/public/admin.php
+++ Live/public/admin.php
@@ -85,26 +85,6 @@
 
 
 
-    <!-- Javascript -->
-    
-    <script>
-        function initMap(){
-            var location = {lat: 59.913869, lng: 10.752245};
-            var mapOptions = {
-                zoom: 14, 
-                left: location,
-                scrollwheel: false
-            }
-            
-            var map = new google.maps.Map(document.getElementById('map'),mapOptions
-            )
-            
-            var marker = new google.maps.Marker({
-                position: location,
-                map: map
-            })
-            }; 
-    </script>
     
    <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAUOSBwWQ1fKQHF-40kF5kK4h_h4VYA46U&callback=initMap">
