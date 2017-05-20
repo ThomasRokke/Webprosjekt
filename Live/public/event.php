0c9238bed60aede74106b0d92c5b224a6bbc2427
Christian Thorby <christianthorby@hotmail.com>
2017-05-20 14:43:03 +0200

    lagt til dropdown meny som erstatter den vanlige menyen når skjerbredde er 850px eller mindre

--- Live/public/event.php
+++ Live/public/event.php
@@ -45,6 +45,8 @@
 
 <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAUOSBwWQ1fKQHF-40kF5kK4h_h4VYA46U"></script>
+<script src="js/dropdown.js" type="text/javascript">
+</script>
 
 <script src="js/eventKart.js" type="text/javascript">
 </script>
