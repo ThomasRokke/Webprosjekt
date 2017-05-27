805e427a8b555afd8ad29a10fb1fda1d85dbc784
Røkke <thomas.roekke@gmail.com>
2017-05-27 15:11:49 +0200

    Dump fra server - slik at vi kan oppdatere filene

---
+++ Innlevering/Includes/getOpenBox.php
@@ -0,0 +1,283 @@
+<?php
+
+/**
+
+ * Created by PhpStorm.
+
+ * User: Thomas
+
+ * Date: 15.05.2017
+
+ * Time: 10.53
+
+ */
+
+
+
+require_once('../Includes/db_tilkobling.php');
+
+
+
+//Setter riktig tidssone
+
+date_default_timezone_set('Europe/Berlin');
+
+
+
+$engDay = date('l');
+
+
+
+$time = 150000; // date('His');
+
+
+
+
+
+
+
+$day = "";
+
+
+
+switch($engDay) {
+
+    case "Monday":
+
+        $day = "Mandag";
+
+        break;
+
+    case "Tuesday":
+
+        $day = "Tirsdag";
+
+        break;
+
+    case "Wednesday":
+
+        $day = "Onsdag";
+
+        break;
+
+    case "Thursday":
+
+        $day = "Torsdag";
+
+        break;
+
+    case "Friday":
+
+        $day = "Fredag";
+
+        break;
+
+    case "Saturday":
+
+        $day = "Lørdag";
+
+        break;
+
+    case "Sunday":
+
+        $day = "Søndag";
+
+        break;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+    if($time > 000000 && $time < 050000 ){
+
+
+
+        switch($engDay) {
+
+            case "Monday":
+
+                $day = "Søndag";
+
+                break;
+
+            case "Tuesday":
+
+                $day = "Mandag";
+
+                break;
+
+            case "Wednesday":
+
+                $day = "Tirsdag";
+
+                break;
+
+            case "Thursday":
+
+                $day = "Onsdag";
+
+                break;
+
+            case "Friday":
+
+                $day = "Torsdag";
+
+                break;
+
+            case "Saturday":
+
+                $day = "Fredag";
+
+                break;
+
+            case "Sunday":
+
+                $day = "Lørdag";
+
+                break;
+
+
+
+        }
+
+
+
+        $query = "SELECT * FROM markers AS ma join OpeningHours AS oh on ma.id = oh.BarId JOIN Days AS da on
+
+        oh.DayId = da.DayId WHERE oh.EndTime > 000000 && oh.EndTime < 050000 && da.Weekday = '$day'";
+
+        $response = @mysqli_query($dbc, $query);
+
+    }
+
+    else{
+
+        $query = "SELECT * FROM markers AS ma join OpeningHours AS oh on ma.id = oh.BarId JOIN Days AS da on
+
+        oh.DayId = da.DayId WHERE (oh.EndTime > $time || oh.EndTime BETWEEN 000000 AND 050000) && oh.StartTime < $time && da.Weekday = '$day';";
+
+        $response = @mysqli_query($dbc, $query);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+# City = bilde, first_name = tittel, last_name = beskrivelse
+
+if($response){
+
+// mysqli_fetch_array will return a row of data from the query
+
+// until no further data is available
+
+
+
+    while($row = mysqli_fetch_array($response)) {
+
+
+
+
+
+
+
+
+
+        echo '<div class="CTbox" id="festbox">' .
+
+            '<div class="CTbox-image">' .
+
+            '<h3 class="boksStenger">' .
+
+            'Stenger: ' . $row['EndTime'] .
+
+            '</h3>' .
+
+            '<img src="Images/'.
+
+            $row['imagepath'] .
+
+            '.JPG">' .
+
+            '</div>' .
+
+            '<div class="CTbox-info">' .
+
+            '<h4>'.
+
+            $row['name'].
+
+            '</h4>' .
+
+            '<p>'.
+
+            $row['sDesc']. '<br>' .
+
+
+
+
+
+            '</p>' .
+
+            '<a class="CTbutton" href="merinfo.php?id=' .
+
+            $row['id'] .
+
+            '#merInfoAnchor">Mer info!</a>' .
+
+            '</div>' .
+
+            '</div>';
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
+else {
+
+
+
+    echo "Kunne ikke hente data fra databasen<br />";
+
+
+
+    echo mysqli_error($dbc);
+
+
+
+}
+
+
+
+// Close connection to the database
+
+mysqli_close($dbc);
---
+++ Innlevering/public_html/aktiviteter.php
@@ -0,0 +1,119 @@
+<?php include("../Includes/head.php"); ?>
+
+
+
+<div class="top-image">
+
+
+
+    <?php include("../Includes/menu.php"); ?>
+
+    <?php include("logo.php"); ?>
+
+</div>
+
+
+
+<section class="main-content">
+
+
+
+    <section class="topSection">
+
+        <article class="headArticle">
+
+            <h2>Aktiviteter</h2>
+
+            <p>Tamen eiusmod graviterque id se sunt tamen labore admodum, fugiat a ab quorum
+
+                vidisse ne incididunt dolor non ingeniis illustriora ex ingeniis quid ex
+
+                arbitror arbitrantur, magna ita possumus de velit, eu ipsum ingeniis, qui a
+
+                despicationes. Consequat est eiusmod, ubi sint     commodo est tamen appellat. Deserunt quo fugiat laborum, malis mentitum
+
+            </p>
+
+        </article>
+
+
+
+        <div id="google_map"></div>
+
+
+
+    </section>
+
+
+
+    <section class="parallax">
+
+
+
+    </section>
+
+
+
+    <section class="box_section">
+
+        <?php
+
+        $phpType = 'aktivitet';
+
+        require_once('../Includes/getBox.php');
+
+        ?>
+
+    </section>
+
+
+
+
+
+</section>
+
+
+
+
+
+<?php include("footer.php") ?>
+
+
+
+
+
+
+
+
+
+
+
+<!-- Javascript -->
+
+
+
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAUOSBwWQ1fKQHF-40kF5kK4h_h4VYA46U"></script>
+
+
+
+<script src="js/aktivitet.js" type="text/javascript">
+
+</script>
+
+<script src="js/dropdown.js" type="text/javascript">
+
+</script>
+
+
+
+<audio id="audio" src="lyd.mp3" ></audio>
+
+
+
+
+
+</body>
+
+</html>
