e49aa1813dd0574ded44bac83c3687efadacd76e
Røkke <thomas.roekke@gmail.com>
2017-05-27 16:14:10 +0200

    Fix og update på getOpenBox

--- Innlevering/Includes/db_tilkobling.php
+++ Innlevering/Includes/db_tilkobling.php
@@ -1,11 +1,19 @@
 <?php
 
     
-
+/*
 DEFINE('DB_USER', 'roktho16_thomas');
 DEFINE('DB_PASSWORD', 'MU8GTHJBXE66');
 DEFINE('DB_HOST', 'localhost:3306');
 DEFINE('DB_NAME', 'roktho16_wzup');
+
+*/
+
+DEFINE('DB_USER', 'my_cms');
+DEFINE('DB_PASSWORD', 'webprosjekt');
+DEFINE('DB_HOST', 'localhost');
+DEFINE('DB_NAME', 'wzup');
+
 
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
