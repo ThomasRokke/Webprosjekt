acac42f5c2d711f12632e1675749cde3f4799395
Røkke <thomas.roekke@gmail.com>
2017-05-28 21:18:15 +0200

    Endring av DB_tilkobling

--- Innlevering/Includes/db_tilkobling.php
+++ Innlevering/Includes/db_tilkobling.php
@@ -1,18 +1,12 @@
 <?php
 
     
-/*
+
 DEFINE('DB_USER', 'roktho16_thomas');
-DEFINE('DB_PASSWORD', 'MU8GTHJBXE66');
+DEFINE('DB_PASSWORD', '********'); //Fjernet av sikkerhetsmessige årsaker
 DEFINE('DB_HOST', 'localhost:3306');
 DEFINE('DB_NAME', 'roktho16_wzup');
 
-*/
-
-DEFINE('DB_USER', 'my_cms');
-DEFINE('DB_PASSWORD', 'webprosjekt');
-DEFINE('DB_HOST', 'localhost');
-DEFINE('DB_NAME', 'wzup');
 
 
 
